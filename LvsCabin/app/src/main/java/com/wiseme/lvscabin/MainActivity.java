d62721c5e2c1448949f4844afc572ffa957e370d
lvtoa <lxt352@gmail.com>
2016-09-27 17:26:16 +0800

    add bottombar libaray

--- LvsCabin/app/src/main/java/com/wiseme/lvscabin/MainActivity.java
+++ LvsCabin/app/src/main/java/com/wiseme/lvscabin/MainActivity.java
@@ -2,6 +2,10 @@ package com.wiseme.lvscabin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
