1774dc55b09620e8516fa06f1cac20276541fe0f
lxt <lxt352@gmail.com>
2017-11-14 11:13:15 +0800

    Add kotlin support

---
+++ app/src/main/java/com/wiseme/lvscabin/kotlin/view/OutlineProvider.kt
@@ -0,0 +1,19 @@
+package com.wiseme.lvscabin.kotlin.view
+
+import android.graphics.Outline
+import android.graphics.Rect
+import android.view.View
+import android.view.ViewOutlineProvider
+
+/**
+ * @author lxt <lxt352@gmail.com>
+ * @since 2017/11/14.
+ */
+class OutlineProvider(private val rect: Rect = Rect(),
+                      var scaleX: Float,
+                      var scaleY: Float,
+                      var yShift: Int) : ViewOutlineProvider() {
+
+    override fun getOutline(view: View?, outline: Outline?) {
+    }
+}
